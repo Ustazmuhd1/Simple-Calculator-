3be77fcfc4b5356d6febe4082e43475ce1a341d5
Ustazmuhd1 <muhammadabdulhadishamun22@gmail.com>
2025-05-05 14:47:38 -0700

    Add files via upload
    Added calculator script

---
+++ calculator.py
@@ -0,0 +1,12 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def add(a, b):
+    return a + b
+
+print("Sum:", add(10, 5))
+
+SyntaxError: invalid syntax
+def add(a, b):
+    return a + b
+
+print("Sum:", add(10, 5))
